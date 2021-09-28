846ef320a063995739e780f1cd322e7edb8252b3
LeAnhTuan59/webApplication1 <tenlatuan1002@gmail.com>
2021-09-28 10:22:42 +0700

    Add project files.

---
+++ PTB2/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BaiNgay7hang9.Models
+{
+    public class Account
+    {
+        [Key]
+        public string Usename { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+
+    }
+}
